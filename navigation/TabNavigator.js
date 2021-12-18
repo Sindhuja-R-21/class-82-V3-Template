82634023922011733b2573207ff020cfdfb93136
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2021-12-18 16:43:54 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Feed from "../screens/Feed";
+import CreateStory from "../screens/CreateStory";
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Create Story" component={CreateStory} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "#2f345d",
+    height: "8%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: "hidden",
+    position: "absolute"
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30)
+  }
+});
+
+export default BottomTabNavigator;
